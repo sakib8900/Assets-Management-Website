00e1239bbbaef58dc4359e4171b295d2a5512b24
sakib8900 <md.sakib388900@gmail.com>
2025-01-19 22:43:38 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <button className='btn btn-error'>button</button>
+      <h3 className='text-4xl'>Hello</h3>
+    </div>
+  );
+};
+
+export default App;
